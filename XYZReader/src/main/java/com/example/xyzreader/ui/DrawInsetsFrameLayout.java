842af627e2ba55225c51ff8bb4ba2d04b90134f7
Alan Smillie <alan.smillie@gmail.com>
2019-02-20 09:27:21 -0500

    Refactor method to account for function call that is only available to API 20+

--- XYZReader/src/main/java/com/example/xyzreader/ui/DrawInsetsFrameLayout.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/DrawInsetsFrameLayout.java
@@ -103,16 +103,20 @@ public class DrawInsetsFrameLayout extends FrameLayout {
     @Override
     public WindowInsets onApplyWindowInsets(WindowInsets insets) {
         insets = super.onApplyWindowInsets(insets);
-        mInsets = new Rect(
-                insets.getSystemWindowInsetLeft(),
-                insets.getSystemWindowInsetTop(),
-                insets.getSystemWindowInsetRight(),
-                insets.getSystemWindowInsetBottom());
-        setWillNotDraw(false);
-        postInvalidateOnAnimation();
-        if (mOnInsetsCallback != null) {
-            mOnInsetsCallback.onInsetsChanged(mInsets);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT_WATCH) {
+            mInsets = new Rect(
+                    insets.getSystemWindowInsetLeft(),
+                    insets.getSystemWindowInsetTop(),
+                    insets.getSystemWindowInsetRight(),
+                    insets.getSystemWindowInsetBottom());
+
+            setWillNotDraw(false);
+            postInvalidateOnAnimation();
+            if (mOnInsetsCallback != null) {
+                mOnInsetsCallback.onInsetsChanged(mInsets);
+            }
         }
+
         return insets;
     }
 
